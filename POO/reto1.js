6d7dbfdbcacba0b2790a067a40a3a545305230e5
MMSS97 <michaelsorrosa@gmail.com>
2024-11-16 22:06:49 +0100

    personas nuevas

--- POO/reto1.js
+++ POO/reto1.js
@@ -8,4 +8,9 @@ class persona {
     }
 }
 let persona1= new persona("marcos","jimenes",15,"castaño","azules")
+let persona2= new persona("Irene","Mosca",17,"rubio","azules")
+let persona3= new persona("Ricardo","alonso",13,"negro","negros")
 console.log(persona1)
+console.log(persona2)
+console.log(persona3)
+
