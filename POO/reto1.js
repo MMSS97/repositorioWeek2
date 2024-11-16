143bfa2b15ca049c882d0c2ad141b029c6f93d73
MMSS97 <michaelsorrosa@gmail.com>
2024-11-16 14:00:39 +0100

    dia1 personas

---
+++ POO/reto1.js
@@ -0,0 +1,11 @@
+class persona {
+    constructor (nombre, apellido, edad, colorDePelo, colorDeOjos){
+        this.nombre= nombre
+        this.apellido=apellido
+        this.edad=edad
+        this.colorDePelo=colorDePelo
+        this.colorDeOjos=colorDeOjos
+    }
+}
+let persona1= new persona("marcos","jimenes",15,"castaño","azules")
+console.log(persona1)
